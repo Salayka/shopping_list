76fdfd9cd0e766e7593a3d8c9ef206b54df4a924
George Salayka <george.salayka@gmail.com>
2025-12-13 23:03:05 -0500

    ensured that there was a load spinner while sending and multiple sends disabled.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -18,10 +18,14 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
       //save this to the database
       final url = Uri.https(
         'shopping-list-d36b7-default-rtdb.firebaseio.com',
@@ -134,12 +138,19 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: const Text("Reset"),
                   ),
-                  ElevatedButton(onPressed: _saveItem, child: Text("Add Item")),
+                  ElevatedButton(
+                    onPressed: _isSending ? null : _saveItem,
+                    child:_isSending ? SizedBox(
+                      height:16, width:16, child: CircularProgressIndicator(),
+                    ): Text("Add Item"),
+                  ),
                 ],
               ),
             ],
